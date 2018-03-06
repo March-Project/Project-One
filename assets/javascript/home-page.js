63a222d73455e2bf6a17c0af0d4d63ee05cc3b9c
kayla mcfadden <kmcfadden0741@gmail.com>
2018-03-06 10:22:41 -0500

    added sellers page and a navbar for search functions

--- assets/javascript/home-page.js
+++ assets/javascript/home-page.js
@@ -53,7 +53,7 @@ function onSearchClick(){
 
 
 
- var map = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('mapid').setView([51.505, -0.09], 13);
  
  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
