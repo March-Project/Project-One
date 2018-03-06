3ae64f8d4d87dc743fa33d4f77f93dc5f9fea587
kayla mcfadden <kmcfadden0741@gmail.com>
2018-03-05 20:47:42 -0500

    created new layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,124 @@
-//var googleApiKey = AIzaSyBVw0nXlHEyYbC6WWsyTjm_IPDQBze0Jlo;
-//var googleLink =
- //"https://maps.googleapis.com/maps/api/js?key=" +
- //googleApiKey +
- //"&callback=initMap";
-// Initialize Firebase
-// Initialize Firebase
 
-var config = {
- apiKey: "AIzaSyB0p6U_W374WOOQ2H-TNBzNJ1GiAZl-tHQ",
- authDomain: "project-one-march.firebaseapp.com",
- databaseURL: "https://project-one-march.firebaseio.com",
- projectId: "project-one-march",
- storageBucket: "",
- messagingSenderId: "756241675187"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-var name = "";
-var hours;
-var goods;
-// var nextArrival;
-// var minutesAway;
-$("#submit").on("click", function() {
- event.preventDefault();
- name = $("#name").val();
- destination = $("#destination").val();
- hours = $("#hours").val();
- goods = $("#goods").val();
-  
- // Clears the form after pressing submit
- $("#name").val("");
- $("#destination").val("");
- $("#hours").val("");
- $("#goods").val("");
- database.ref().push({
-   name: name,
-   destination: destination,
-   hours: hours,
-   goods: goods,
-   TIMESTAMP: firebase.database.ServerValue.TIMESTAMP
- });
+var apiKey = "CnHguXgYZvGys0UUBWq3MKFTXEwOI5NI";
+
+var topics = ["Joyful", "Peaceful", "Rageful", "Friendly", "Rebellious", "Hilarious"];
+
+
+$(document).ready(function(){
+   $("#search-descriptor").on("click", onSearchClick);
+
+   for(var i = 0; i < topics.length; i++) {
+      addTopicButton(topics[i]);
+   }
+
+   $("#buttons").on("click", "button", displayGifs);
+
+   clearGif(); 
+
+    $(document).on("click", ".newImg", function(){
+          var gifState = $(this).attr("data-state");
+          console.log("newImg");
+            
+         if (gifState === "still") {
+            console.log("stateanimate");
+            var gifAnimate = $(this).attr("data-animate");
+               $(this).attr("src", gifAnimate);
+               $(this).attr("data-state", "animate");
+
+            } else {
+               console.log("statestill");
+            var gifStill = $(this).attr("data-still");
+            $(this).attr("src", gifStill);
+            $(this).attr("data-state", "still");
+
+      }
+   })
+
 });
-database.ref().on(
- "child_added",
- function(snapshot) {
-   var snap = snapshot.val();
-   console.log(snap.name);
-   console.log(snap.destination);
-   console.log(hours);
-   console.log(snap.goods);
-  
-   $("#name-display").text(snap.name);
-   $("#destination-display").text(snap.destination);
-   $("#hours-display").text(snap.hours);
-   $("#goods-display").text(snap.goods);
 
 
-   var tBody = $("#dataTable");
-   var tRow = $("<tr>");
-   var nameTd = $("<td>").text(snap.name);
-   var destinationTd = $("<td>").text(snap.destination);
-   var hoursTd = $("<td>").text(snap.hours);
-   var goodsTd = $("<td>").text(snap.goods);
+// On click, giphy is displayed. 
+function displayGifs(){
+   var topic = $(this).attr("data-topic");
+   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
 
-   // Append the newly created table data to the table row
-   tRow.append(nameTd, destinationTd, hoursTd, goodsTd);
-   // Append the table row to the table body
-   tBody.append(tRow);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+            console.log(response);
+               gifVal(response);
+            
+            
+      });
 
- },
- function(errorObject) {
-   console.log("Errors handled: " + errorObject.code);
- }
-);
+}  
+
+//Creates the rating and state of the gif
+function gifVal(response){
+
+
+   var results = response.data;
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            }
+    var rating = results[i].rating;
+    var p = $("<p>").text("Rating: " + rating);
+
+   var newGif = $("<img>");
+      newGif.attr("src", response.data[i].images.fixed_height_still.url);
+      newGif.attr("data-still", response.data[i].images.fixed_height_still.url); 
+        newGif.attr("data-animate", response.data[i].images.fixed_height.url); 
+        newGif.attr("data-state", "still");
+        newGif.addClass("newImg");
+        $("#gifs").append(newGif); 
+        $("#gifs").append(p);
+
+  }
+}
+
+// grabs value from text box and adds to array 
+function getSearchVal(){
+   var searchBox = $("#topic-input");
+   var newTopic = searchBox.val();
+   topics.push(newTopic);
+   searchBox.val("");
+
+   return newTopic;
+}
+
+// creates new button with text box value and appends html.
+function addTopicButton(newTopic){
+   var topicButton = $("<button>");
+   topicButton.text(newTopic);
+   topicButton.attr("data-topic", newTopic);
+
+   $("#buttons").append(topicButton);
+}
+
+//when Search is clicked, button with text is added to the browser.
+function onSearchClick(){
+   var searchVal = getSearchVal();
+   addTopicButton(searchVal);
+}
+
+function clearGif(){
+   $("#clear-gifs").on("click", function(){
+        event.preventDefault();
+        $("#gifs").empty(); 
+
+})
+}
+
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+    console.log(map);
